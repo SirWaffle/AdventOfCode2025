499f66792a4e811268736f7f28bb26bfb3d3fe57
SirWaffle <18519779+SirWaffle@users.noreply.github.com>
2025-12-03 08:14:01 -0600

    bleh

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -10,77 +10,65 @@ namespace AdventOfCode2025.Days
 {
     public class Day1: Day_base
     {
-        public Day1() { }
-
-        public class Dial
+        public override void Solve()
         {
-            public int count { get; set; } = 0;
-
-            int val = 50;            
-            int exclusive_max = 100;
-            bool countClicks = false;
-
-            public Dial(int val, int exclusive_max, bool countClicks)
-            {
-                this.val = val;
-                this.exclusive_max = exclusive_max;
-                this.countClicks = countClicks;
-            }
-
-            public void Turn(int turn)
-            {
-                val += turn;
-
-                if (val == 0)
-                    count++;
-
-                while (val < 0)
-                {
-                    val += exclusive_max;
-                    if(countClicks)
-                        count++;
-                }
-
-                while (val >= exclusive_max)
-                {
-                    val -= exclusive_max;
-                    if (countClicks)
-                        count++;
-                }                   
-            }
-        };
+            Solve_P2_linq();
+        }
 
         public void Solve_P1()
         {
             Console.WriteLine(File.ReadAllText(@"Days\" + this.GetType().Name + "_example.txt").Split(Environment.NewLine).Select(x => x.Trim().ToLower()).Select(x => int.Parse(x.Substring(1)) * (x.ToLower()[0] == 'l' ? -1 : 1)).Select(x => (0, 0, x)).Prepend((50, 0, 0)).Aggregate((accum, item) => ((accum.Item1 + item.Item3 ) % 100, accum.Item2 + (((accum.Item1 + item.Item3) % 100) == 0 ? 1 : 0), 0)).Item2);
         }
 
-        public void Solve_P2()
+        public void Solve_P2_linq()
         {
+            Console.WriteLine("answer: " + File.ReadAllText(@"Days\" + this.GetType().Name + "_example.txt")
+                .Split(Environment.NewLine).Select(x => x.Trim().ToLower())
+                .Select(x => ((x.ToLower()[0] == 'l' ? -1 : 1), int.Parse(x.Substring(1))))
+                .Select(item => Enumerable.Range(0, item.Item2).Select(x => item.Item1).ToList())
+                .Aggregate((accum, item) => accum.Concat(item).ToList())
+                .Select(x => (0, x, 0)).Prepend((50, 0, 0))
+                .Aggregate((accum, x) => ((accum.Item1 + x.Item2) % 100, x.Item2, (((accum.Item1 + x.Item2) % 100) == 0 ? accum.Item3 + 1 : accum.Item3))).Item3);              
+        }
 
-            //List<string> input = ReadWholeInput().Split(Environment.NewLine).Select(x => x.Trim().ToLower()).ToList();
+        public void Solve_P2() // 5941
+        {
+            var turns = ReadWholeInput().Split(Environment.NewLine).Select(x => x.Trim().ToLower()).Select(x => int.Parse(x.Substring(1)) * (x.ToLower()[0] == 'l' ? -1 : 1));
+            turns.ToList();
 
-            //Dial dial = new Dial(50, 100, true);
-            //input.ForEach(x => dial.Turn(int.Parse(x.Substring(1)) * (x.ToLower()[0] == 'l' ? -1 : 1)));
+            int dial = 50;
+            int clicks = 0;
+            foreach(var turn in turns)
+            {
+                for (int i = 0; i < Math.Abs(turn); ++i)
+                {
+                    int inc = Math.Sign(turn) * 1;
 
-            //Console.WriteLine(dial.count); // too high 5949
+                    dial += inc;
+                    if (dial == 100)
+                        dial = 0;
+                    if (dial == -1)
+                        dial = 99;
+                    if (dial == 0)
+                        clicks++;
+                }
+            }
 
-            Console.WriteLine(
+            Console.WriteLine("answer: " + clicks);
+            /*
+            Console.WriteLine( "answer: " + 
                 File.ReadAllText(@"Days\" + this.GetType().Name + "_input.txt")
                     .Split(Environment.NewLine)
                     .Select(x => x.Trim().ToLower())
                     .Select(x => int.Parse(x.Substring(1)) * (x.ToLower()[0] == 'l' ? -1 : 1))
-                    .Select(x => (0, 0, x))
-                    .Prepend((50, 0, 0))
-                    .Aggregate((accum, item) =>(
-                                                   (accum.Item1 + item.Item3) % 100
-                                                  , accum.Item2 + (int)Math.Truncate(Math.Abs((float)(accum.Item1 + item.Item3) / 100.0f))
-                                                    + ((accum.Item1 + item.Item3 != 0 && Math.Sign((decimal)accum.Item1) != Math.Sign((accum.Item1 + item.Item3)))? 1: 0)
-                                                    + ((accum.Item1 + item.Item3 == 0 && !(Math.Sign((decimal)accum.Item1) != Math.Sign((accum.Item1 + item.Item3)))) ? 1 : 0)
-                                                  , 0
-                                               )
-                ).Item2
-            );
+                    .Select(x => (0, 0, x, Enumerable.Repeat( x> 0? 1:-1, Math.Abs(x)).ToList()))
+                    .Prepend((50,0,0,new List<int>()))
+                    .Aggregate( (accum, state) => )
+
+                    .Select( (accum, state) => Enumerable.Range(0, Math.Abs(state.Item3))
+                                              .Select(x => (state.Item1
+                    .Item2
+            );*/
         }
     }
 }
